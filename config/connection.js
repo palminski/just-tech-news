2bc2dcdeba3aba851cf771ba6b22919248d7162d
palminski <williambolls98@gmail.com>
2022-09-28 19:32:22 -0700

    changed back

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB) {
-    sequelize = new Sequelize(process.env.JAWSDB);
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 }
 else {
     sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD, {
