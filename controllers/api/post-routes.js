cb7ad866ba82c519be341fd5a0a98027f92770cd
palminski <williambolls98@gmail.com>
2022-10-05 14:55:37 -0700

    added ability to upvote posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -95,12 +95,14 @@ router.post('/', (req,res) => {
 //PUT /api/posts/updoot
 router.put('/upvote', (req, res) => {
     //custom static method
-    Post.upvote(req.body, {Vote})
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+    if(req.session) {
+        Post.upvote({ ...req.body, user_id: req.session.user_id}, {Vote, Comment, User})
+        .then(updatedVoteData => res.json(updatedVoteData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
 });
 
 //put change title
