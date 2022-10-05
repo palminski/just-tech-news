2e74cceb3ab4b7596353c4c622f154610bfc6d35
palminski <williambolls98@gmail.com>
2022-10-05 15:30:25 -0700

    partway through 3.5

---
+++ public/javascript/comment.js
@@ -0,0 +1,12 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+
+    const comment_text = document.querySelector('textarea[name="comment-body"]').ariaValueMax.trim();
+    const post_id = window.location.toString().split('/')[ //gets url split at the /
+        window.location.toString().split('/').length -1
+    ];
+
+    console.log(comment_text, post_id);
+} 
+
+document.querySelector('.comment-form').addEventListener('submit',commentFormHandler);
