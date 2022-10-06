a4dcbba9419ae50fb25bc9ef05596a9c0c578062
palminski <williambolls98@gmail.com>
2022-10-06 11:19:37 -0700

    finished helper functions

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -26,7 +27,8 @@ app.use(session(sess));
 
 //For Handlebars
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
